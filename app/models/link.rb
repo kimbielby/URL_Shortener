26a0e3daf0361d65e71980c9f82a177244045660
Kim Bielby <kb_f5@yahoo.com>
2016-06-05 20:35:42 +0200

    move logic from model to controller for iteration one

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,7 +1,2 @@
 class Link < ActiveRecord::Base
-  def self.redirection(arg)
-    link_id = Link.find_by(short_URL: arg).id
-    original_link = Link.find_by(id: link_id).original_URL
-    redirect_to 'http://' + original_link
-  end
 end
